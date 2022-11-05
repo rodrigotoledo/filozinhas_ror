b2ad7e199b17806f1229bed24ce95aa8a1202f53
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2022-11-05 15:22:16 -0300

    api products without devise

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,4 +1,5 @@
 class Api::ProductsController < ApplicationController
+  skip_before_action :authenticate_user!
   before_action :set_product, only: %i[ show  ]
 
   # GET /products or /products.json
